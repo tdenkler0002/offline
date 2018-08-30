8503ec81fe5711010984dd08bcc2d568152f78ce
David Smith <david.lsmith@gmail.com>
2018-08-30 09:24:43 -0700

    Added character placeholder; character movement

--- src/main.js
+++ src/main.js
@@ -16,20 +16,11 @@ class MapManager {
         this.canvas = document.createElement('canvas');
         this.canvas.width = this.width;
         this.canvas.height = this.height;
-        this.context = canvas.getContext('2d');
+        this.context = this.canvas.getContext('2d');
         this.tiles = [];
-
-        this.map = [
-            [1, 1, 1, 1],
-            [1, 0, 1, 1],
-            [1, 1, 1, 0],
-            [1, 1, 1, 1, 1, 1],
-            [1, 0, 1, 1, 1, 1],
-            [1, 1, 1, 0, 1, 1]
-        ];
     }
 
-    createFace(coords, fill, stroke) {
+    drawFace(coords, fill, stroke) {
         this.context.beginPath();
         this.context.fillStyle = fill;
         this.context.strokeStyle = stroke;
@@ -57,30 +48,40 @@ class MapManager {
                 continue;
             }
 
-            this.createFace([tile.x + xMin, tile.y, tile.x, tile.y + yMid, tile.x + xMid, tile.y + yMid, tile.x + xMax, tile.y], '#fff', '#8e8e8e');
-            this.createFace([tile.x + xMax, tile.y, tile.x + xMax, tile.y + yMin, tile.x + xMid, tile.y + yMax, tile.x + xMid, tile.y + yMid], '#807f7e', '#666');
-            this.createFace([tile.x + xMid, tile.y + yMid, tile.x + xMid, tile.y + yMax, tile.x, tile.y + yMax, tile.x, tile.y + yMid], '#959492', '#c6b6a6');
+            this.drawFace([tile.x + xMin, tile.y, tile.x, tile.y + yMid, tile.x + xMid, tile.y + yMid, tile.x + xMax, tile.y], '#fff', '#8e8e8e');
+            this.drawFace([tile.x + xMax, tile.y, tile.x + xMax, tile.y + yMin, tile.x + xMid, tile.y + yMax, tile.x + xMid, tile.y + yMid], '#807f7e', '#666');
+            this.drawFace([tile.x + xMid, tile.y + yMid, tile.x + xMid, tile.y + yMax, tile.x, tile.y + yMax, tile.x, tile.y + yMid], '#959492', '#c6b6a6');
         }
     }
 
-    buildMap() {
+    init() {
         let x = 0;
         let y = 0;
-        let xOffset = 41;
+        let xOffset = -31;
         let xsize = 71;
-        const ysize = 41;
+        let ysize = 41;
 
-        for (let i = 0; i < this.map.length; i++) {
-            x = xOffset * i;
-            for (let j = 0; j < this.map[i].length; j++) {
-                x += xsize;
+        for (let i = 0; i < map.length; i++) {
+            for (let j = 0; j < map[i].length; j++) {
+                const tile = new Tile(x, y, map[i][j]);
 
-                const tile = new Tile(x, y, this.map[i][j]);
+                map[i][j] = { id: j, x: x, y: y, xsize: xsize, ysize: ysize, type: map[i][j] };
 
                 this.tiles.push(tile);
+
+                x += xsize;
+
+                if (j > maxXIndex) {
+                    maxXIndex = j;
+                }
             }
 
+            x = xOffset;
             y += ysize;
+
+            if (i > maxYIndex) {
+                maxYIndex = i;
+            }
         }
 
         this.drawTiles();
@@ -91,21 +92,150 @@ class MapManager {
     }
 }
 
+class Character {
+    constructor() {
+        this.x;
+        this.y;
+        this.width = 100;
+        this.height = 100;
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.context = this.canvas.getContext('2d');
+    }
+
+    drawFace(coords, fill, stroke) {
+        this.context.save();
+        this.context.scale(0.5, 0.5);
+        this.context.beginPath();
+        this.context.fillStyle = fill;
+        this.context.strokeStyle = stroke;
+        this.context.moveTo(coords[0], coords[1]);
+        this.context.lineTo(coords[2], coords[3]);
+        this.context.lineTo(coords[4], coords[5]);
+        this.context.lineTo(coords[6], coords[7]);
+        this.context.stroke();
+        this.context.fill();
+        this.context.closePath();
+        this.context.restore();
+    }
+
+    init() {
+        const xMin = 30;
+        const xMid = 70;
+        const xMax = 100;
+        const yMin = 20;
+        const yMid = 40;
+        const yMax = 60;
+
+
+        const startX = 0;
+        const startY = 20;
+
+        this.drawFace([startX + xMin, startY, startX, startY + yMid, startX + xMid, startY + yMid, startX + xMax, startY], '#83d30b', '#4d7610');
+        this.drawFace([startX + xMax, startY, startX + xMax, startY + yMin, startX + xMid, startY + yMax, startX + xMid, startY + yMid], '#69aa07', '#599106');
+        this.drawFace([startX + xMid, startY + yMid, startX + xMid, startY + yMax, startX, startY + yMax, startX, startY + yMid], '#58890f', '#58821a');
+    }
+
+    getCharacterCanvas() {
+        return this.canvas;
+    }
+
+}
+
 const canvas = document.getElementById('game');
 canvas.width = 900;
 canvas.height = 600;
 
-const context = canvas.getContext('2d');
+const gameContext = canvas.getContext('2d');
 const mapManager = new MapManager(canvas.width, canvas.height);
+const character = new Character();
+
+const map = [
+    [1, 1, 1, 1],
+    [0, 1, 1, 1, 1],
+    [0, 0, 1, 1, 1]
+];
+
+let xIndex = 0;
+let yIndex = 0;
+let coord;
+let maxXIndex = 0;
+let maxYIndex = 0;
 
 function draw() {
-    context.drawImage(mapManager.getTileCanvas(), 0, 0);
+    // gameContext.clearRect(0, 0, canvas.width, canvas.height);
+
+    gameContext.drawImage(mapManager.getTileCanvas(), 0, 0);
+    gameContext.drawImage(character.getCharacterCanvas(), character.x, character.y);
 
     requestAnimationFrame(draw);
 }
 
+function addEventListeners() {
+    window.onkeydown = (event) => {
+        navigate(event);
+    };
+}
+
+function navigate(event) {
+    const keyCode = event.keyCode;
+
+    // Navigate only if there is a valid keycode, the character is on the map,
+    // and type is valid
+    switch (keyCode) {
+        case 65:
+            if (xIndex - 1 > -1 && map[yIndex][xIndex - 1].type !== 0) {
+                xIndex -= 1;
+                moveCharacter();
+            }
+            break;
+        case 68:
+            if (xIndex + 1 < maxXIndex + 1 && map[yIndex][xIndex + 1].type !== 0) {
+                xIndex += 1;
+                moveCharacter();
+            }
+            break;
+        case 87:
+            if (yIndex - 1 > -1 && map[yIndex - 1][xIndex].type !== 0) {
+                yIndex -= 1;
+                moveCharacter();
+            }
+            break;
+        case 83:
+            if (yIndex + 1 < maxYIndex + 1 && map[yIndex + 1][xIndex].type !== 0) {
+                yIndex += 1;
+                moveCharacter();
+            }
+            break;
+    }
+}
+
+function moveCharacter() {
+    coord = {
+        x: map[yIndex][xIndex].x,
+        y: map[yIndex][xIndex].y
+    };
+
+    // Some offsets
+    character.x = coord.x + 20;
+    character.y = coord.y - 6;
+}
+
 function init() {
-    mapManager.buildMap();
+    mapManager.init();
+    character.init();
+
+    coord = {
+        x: map[xIndex][yIndex].x,
+        y: map[xIndex][yIndex].y
+    };
+
+    // Set character at start
+    character.x = coord.x + 20;
+    character.y = coord.y - 6;
+
+    addEventListeners();
 
     draw();
 }
